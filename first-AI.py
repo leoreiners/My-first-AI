030994f2e88984d64494bd5d3df017882ad60788
Leonardo Reiners <leoreiners@gmail.com>
2021-07-29 16:58:00 -0400

    Update first-AI.py

--- first-AI.py
+++ first-AI.py
@@ -17,3 +17,15 @@ str_to_num = {'gnome':0, 'drone':1}
 #PT - criando as variáveis dos diretórios EN - creating directory variables
 gnome_folder = 'loading_data/gnome'
 drone_folder = 'loading_data/drone'
+
+#PT - transformando as imagens em matrizes EN - Converting images to arrays
+def create_data(folder, name):
+    for i in os.listdir(folder):
+        image = Image.open(os.path.join(folder, i))
+        image = Image.Image.resize(image, [200, 200])
+        x = np.array(image)
+        X.append(x)
+        y.append(str_to_num[name])
+
+create_data(gnome_folder, 'gnome')
+create_data(drone_folder, 'drone')
